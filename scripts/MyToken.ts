06a6c59056ff8a1bb7c6d0879237dc2fc9483190
iLVino <59415298+iLVino@users.noreply.github.com>
2023-03-05 21:21:38 +0100

    script is finished

--- scripts/MyToken.ts
+++ scripts/MyToken.ts
@@ -1,4 +1,3 @@
-// need have setup for goerly 
 import { ethers } from "ethers";
 import { MyToken__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
@@ -7,11 +6,9 @@ dotenv.config();
 const MINT_VALUE = ethers.utils.parseEther("100");
 
 async function main() {
-    // maybe write down our wallet addresses directly as a const
     //const account1: string = "0x93eC5e12AC770eF01920dF0D870b5A075937b55b";
     //const account2: string = "0x6EB39A5c34C27B109A4Fc6F72CA2E7a380749D29";
-    //const [deployer, account1, account2] = await ethers.getSigners();
-
+   
      // setup for wallet access
     const privateKey = process.env.PRIVATE_KEY;
     if (!privateKey || privateKey.length <= 0) {
@@ -30,27 +27,29 @@ async function main() {
     const balance = await deployer.getBalance();
     console.log(`The account ${deployer.address} has a balance of ${balance} wei`);
 
+    // contract ERC20 tokens deployment 
     const contractFactory = new MyToken__factory(deployer);
     const contract = await contractFactory.deploy();
-    const txReceipt = await contract.deployed();
-    const contractAddress = contract.address; //Undefined
-    const blockNumber = await provider.getBlockNumber() ; //Undefined
+    await contract.deployed();
+    const contractAddress = contract.address; 
+    const blockNumber = await provider.getBlockNumber() ;
     console.log(`The ERC20 tokencontract was deployed at address ${contractAddress} on the block number ${blockNumber}`)
 
+    // token minting
     const mintTx = await contract.mint(deployer.address, MINT_VALUE);
     await mintTx.wait();
     
-    const account1Balance = await contract.balanceOf(deployer.address);
-    console.log(`Account1 has ${ethers.utils.formatEther(account1Balance)} tokens`);
-    let account1VotingPower = await contract.getVotes(deployer.address);
-    console.log(`Account1 has initial Voting power of ${ethers.utils.formatEther(account1VotingPower)} units`);
+    const deployerBalance = await contract.balanceOf(deployer.address);
+    console.log(`deployer has ${ethers.utils.formatEther(deployerBalance)} tokens`);
+    let deployerVotingPower = await contract.getVotes(deployer.address);
+    console.log(`Deployer has initial Voting power of ${ethers.utils.formatEther(deployerVotingPower)} units`);
     
     // self delegation
-    const delegateTx = await contract.connect(deployer.address).delegate(deployer.address);
+    const delegateTx = await contract.connect(deployer).delegate(deployer.address);
     await delegateTx.wait();
 
-    account1VotingPower = await contract.getVotes(deployer.address);
-    console.log(`Account1 has updated Voting power of ${ethers.utils.formatEther(account1VotingPower)} units`);
+    deployerVotingPower = await contract.getVotes(deployer.address);
+    console.log(`Deployer has updated Voting power of ${ethers.utils.formatEther(deployerVotingPower)} units`);
 
 
 };
